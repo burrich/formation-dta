f7e40cb076fd6d70f8d671adea26a5a4e4fde764
Nicolas Jezequel <jezequel.n@gmail.com>
2016-04-26 15:51:11 +0200

    Menu

---
+++ pizzeria-console-imperative/src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -0,0 +1,40 @@
+package fr.pizzeria.console;
+
+import java.util.Scanner;
+
+public class PizzeriaAdminConsoleApp {
+
+	public static void main(String[] args) {
+		int choice = 0;
+		
+		while (choice != 99) {
+			System.out.println("***** Pizzeria Administration *****");
+			System.out.println("1. Lister les pizzas");
+			System.out.println("2. Ajouter une nouvelle pizza");
+			System.out.println("3. Mettre à jour une pizza");
+			System.out.println("4. Supprimer une pizza");
+			System.out.println("99. Sortir");
+			
+			Scanner sc = new Scanner(System.in);
+			choice = sc.nextInt();
+			
+			switch(choice) {
+			case 1:
+				System.out.println("Liste des pizzas");
+				break;
+			case 2:
+				System.out.println("Ajout d'une nouvelle pizza");
+				break;
+			case 3:
+				System.out.println("Mise à jour d'une pizza");
+				break;
+			case 4:
+				System.out.println("Suppression d'une pizza");
+				break;
+			default:
+				break;
+			}
+		}	
+	}
+
+}

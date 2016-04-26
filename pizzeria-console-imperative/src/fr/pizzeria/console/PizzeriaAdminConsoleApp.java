e05e0b0abbd36ca0cb5bc060ffaa1832fd5a74f8
Nicolas Jezequel <jezequel.n@gmail.com>
2016-04-26 16:13:39 +0200

    Tableau d'objets pour les pizzas

--- pizzeria-console-imperative/src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ pizzeria-console-imperative/src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class PizzeriaAdminConsoleApp {
 
 	public static void main(String[] args) {
-		String[][] pizzas = {
-				{"0", "PEP", "Pépéroni", "12.50"},
-				{"1", "MAR", "Margherita", "14.00"},
-				{"2", "REI", "La Reine", "11.50"},
-				{"3", "FRO", "Les 4 fromages", "12.00"},
-				{"4", "CAN", "La cannibale", "12.50"},
-				{"5", "SAV", "La savoyarde", "13.00"},
-				{"6", "ORI", "L'orientale", "13.50"},
-				{"7", "IND", "L'indienne", "14.00"}
+		Object[][] pizzas = {
+				{"PEP", "Pépéroni", 12.50},
+				{"MAR", "Margherita", 14.00},
+				{"REI", "La Reine", 11.50},
+				{"FRO", "Les 4 fromages", 12.00},
+				{"CAN", "La cannibale", 12.50},
+				{"SAV", "La savoyarde", 13.00},
+				{"ORI", "L'orientale", 13.50},
+				{"IND", "L'indienne", 14.00}
 		};
 		int choice = 0;
 		
@@ -32,9 +32,9 @@ public class PizzeriaAdminConsoleApp {
 			case 1:
 				System.out.println("Liste des pizzas");
 				for (int i = 0; i < pizzas.length; i++) {
-					System.out.print(pizzas[i][1] + " -> ");
-					System.out.print(pizzas[i][2] + " (");
-					System.out.println(pizzas[i][3] + "€)");
+					System.out.print(pizzas[i][0] + " -> ");
+					System.out.print(pizzas[i][1] + " (");
+					System.out.println(pizzas[i][2] + "€)");
 				}
 				System.out.print("\n");
 				break;

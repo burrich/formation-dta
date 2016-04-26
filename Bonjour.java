73978846b12ca37d2c913a57ce1a7aa6fe09791d
Nicolas Jezequel <jezequel.n@gmail.com>
2016-04-26 13:30:41 +0200

    Fichiers du TP

---
+++ Bonjour.java
@@ -0,0 +1,14 @@
+/**
+ * La classe Bonjour permet d'afficher un hello world
+ * @author Nicolas
+ */
+public class Bonjour {
+	/**
+	 * Fonction principale
+	 * @param args[] arguments en ligne de commande
+	 */
+	public static void main(String args[]) {
+		// Ecrit sur la sortie standart
+		System.out.println("Bonjour	tout le	monde");
+	}
+}

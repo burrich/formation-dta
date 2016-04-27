012c787431ce36a49931f6b872a83db2af599e55
Nicolas Jezequel <jezequel.n@gmail.com>
2016-04-27 14:25:13 +0200

    Refactorisation

--- pizzeria-console-objet/src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ pizzeria-console-objet/src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -1,166 +1,62 @@
 package fr.pizzeria.console;
 
 import java.util.Scanner;
-
 import fr.pizzeria.model.Pizza;
 
+/**
+ * Administration de pizzas en mode console
+ * @author Nicolas
+ */
 public class PizzeriaAdminConsoleApp {
 
+	/**
+	 * Main
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		Pizza pep = new Pizza();
-		pep.id = 0;
-		pep.code = "PEP";
-		pep.nom = "Pépéroni";
-		pep.prix = 12.50;
-		
-		Pizza mar = new Pizza();
-		mar.id = 1;
-		mar.code = "MAR";
-		mar.nom = "Margherita";
-		mar.prix = 14.00;
-		
-		Pizza rein = new Pizza();
-		rein.id = 2;
-		rein.code = "REIN";
-		rein.nom = "La Reine";
-		rein.prix = 11.50;
-		
-		Pizza fro = new Pizza();
-		fro.id = 3;
-		fro.code = "FRO";
-		fro.nom = "La 4 fromages";
-		fro.prix = 12.00;
-		
-		Pizza can = new Pizza();
-		can.id = 4;
-		can.code = "CAN";
-		can.nom = "La cannibale";
-		can.prix = 12.50;
-		
-		Pizza sav = new Pizza();
-		sav.id = 5;
-		sav.code = "SAV";
-		sav.nom = "La savoyarde";
-		sav.prix = 13.00;
-		
-		Pizza ori = new Pizza();
-		ori.id = 6;
-		ori.code = "ORI";
-		ori.nom = "L'orientale";
-		ori.prix = 13.50;
-		
-		Pizza ind = new Pizza();
-		ind.id = 7;
-		ind.code = "IND";
-		ind.nom = "L'indienne";
-		ind.prix = 14.00;
-		
-		Pizza[] pizzas = new Pizza[100];
-		pizzas[0] = pep;
-		pizzas[1] = mar;
-		pizzas[2] = rein;
-		pizzas[3] = fro;
-		pizzas[4] = can;
-		pizzas[5] = sav;
-		pizzas[6] = ori;
-		pizzas[7] = ind;
-		Pizza.nbPizzas = 8;
+		Pizza[] pizzas = genererPizzas();
 		
 		int choice = 0;
-		
 		while (choice != 99) {
-			System.out.println("***** Pizzeria Administration *****");
-			System.out.println("1. Lister les pizzas");
-			System.out.println("2. Ajouter une nouvelle pizza");
-			System.out.println("3. Mettre à jour une pizza");
-			System.out.println("4. Supprimer une pizza");
-			System.out.println("99. Sortir");
-			
+			afficherMenu();
 			Scanner sc = new Scanner(System.in);
 			choice = sc.nextInt();
 			
 			switch(choice) {
 			case 1:
 				System.out.println("Liste des pizzas");
-				for (int i = 0; i < pizzas.length; i++) {
-					if (pizzas[i] != null) {
-						System.out.print(pizzas[i].code + " -> ");
-						System.out.print(pizzas[i].nom + " (");
-						System.out.println(pizzas[i].prix + "€)");
-					}
-				}
+				afficherPizzas(pizzas);
+				System.out.println("------- " + Pizza.nbPizzas + " pizzas créées depuis l’initialisation du programme");
 				System.out.print("\n");
 				break;
 			case 2:
 				System.out.println("Ajout d'une nouvelle pizza");
-				System.out.println("Veuillez saisir le code");
-				String code = sc.next();
-				System.out.println("Veuillez saisir le nom (sans espace)");
-				String name = sc.next();
-				System.out.println("Veuillez saisir le prix");
-				double price = sc.nextDouble();
-				
-				pizzas[Pizza.nbPizzas] = new Pizza();
+				pizzas[Pizza.nbPizzas] = saisiePizza(sc);
 				pizzas[Pizza.nbPizzas].id = Pizza.nbPizzas;
-				pizzas[Pizza.nbPizzas].code = code;
-				pizzas[Pizza.nbPizzas].nom = name;
-				pizzas[Pizza.nbPizzas].prix = price;
 				Pizza.nbPizzas++;
-
+				
 				System.out.print("\n");
 				break;
 			case 3:
 				System.out.println("Mise à jour d'une pizza");
-				for (int i = 0; i < pizzas.length; i++) {
-					if (pizzas[i] != null) {
-						System.out.print(pizzas[i].code + " -> ");
-						System.out.print(pizzas[i].nom + " (");
-						System.out.println(pizzas[i].prix + "€)");
-					}
-				}
+				afficherPizzas(pizzas);
 				System.out.println("Veuillez choisir la pizza à modifier");
 				System.out.println("(99 pour abandonner)");
 				
 				int pizzaChoice = sc.nextInt();
 				if (pizzaChoice != 99) {
-					System.out.println("Ajout d'une nouvelle pizza");
-					System.out.println("Veuillez saisir le code");
-					code = sc.next();
-					System.out.println("Veuillez saisir le nom (sans espace)");
-					name = sc.next();
-					System.out.println("Veuillez saisir le prix");
-					price = sc.nextDouble();
-					
-					pizzas[pizzaChoice].code = code;
-					pizzas[pizzaChoice].nom = name;
-					pizzas[pizzaChoice].prix = price;
+					pizzas[pizzaChoice] = saisiePizza(sc);
 				}
 		
 				System.out.print("\n");
 				break;
 			case 4:
 				System.out.println("Suppression d'une pizza");
-				for (int i = 0; i < pizzas.length; i++) {
-					if (pizzas[i] != null) {
-						System.out.print(pizzas[i].code + " -> ");
-						System.out.print(pizzas[i].nom + " (");
-						System.out.println(pizzas[i].prix + "€)");
-					}
-				}
+				afficherPizzas(pizzas);
 				System.out.println("Veuillez choisir la pizza à suprrimer");
 				System.out.println("(99 pour abandonner)");
+				supprimerPizza(sc, pizzas);
 				
-				pizzaChoice = sc.nextInt();
-				if (pizzaChoice != 99) {
-					int numPizza = pizzaChoice +1;
-					while (pizzas[numPizza] != null) {
-						pizzas[numPizza -1] = pizzas[numPizza];
-						numPizza++;
-					}
-					pizzas[Pizza.nbPizzas-1] = null;
-				}
-				Pizza.nbPizzas--;
-		
 				System.out.print("\n");
 				break;
 			default:
@@ -168,5 +64,88 @@ public class PizzeriaAdminConsoleApp {
 			}
 		}	
 	}
-
+	
+	/**
+	 * Génere un tableau d'objets Pizza
+	 * @return Tableau d'objets Pizza
+	 */
+	private static Pizza[] genererPizzas() {
+		Pizza[] pizzas = new Pizza[100];
+		pizzas[0] = new Pizza(0, "PEP", "Pépéroni", 12.50);
+		pizzas[1] = new Pizza(1, "MAR", "Margherita", 14.50);;
+		pizzas[2] = new Pizza(2, "REIN", "La Reine", 11.50);;
+		pizzas[3] = new Pizza(3, "FRO", "La 4 fromages", 12.00);;
+		pizzas[4] = new Pizza(4, "CAN", "La cannibale", 12.50);;
+		pizzas[5] = new Pizza(5, "SAV", "La savoyarde", 13.00);;
+		pizzas[6] = new Pizza(6, "ORI", "L'orientale", 13.50);;
+		pizzas[7] = new Pizza(7, "IND", "L'indienne", 14.00);;
+		Pizza.nbPizzas = 8;
+		
+		return pizzas;
+	}
+	
+	/**
+	 * Affiche le menu de la pizzeria.
+	 */
+	private static void afficherMenu() {
+		System.out.println("***** Pizzeria Administration *****");
+		System.out.println("1. Lister les pizzas");
+		System.out.println("2. Ajouter une nouvelle pizza");
+		System.out.println("3. Mettre à jour une pizza");
+		System.out.println("4. Supprimer une pizza");
+		System.out.println("99. Sortir");
+	}
+	
+	/**
+	 * Affiche les pizzas.
+	 * @param pizzas Tableau des Pizza
+	 */
+	private static void afficherPizzas(Pizza[] pizzas) {
+		for (int i = 0; i < pizzas.length; i++) {
+			if (pizzas[i] != null) {
+				System.out.print(pizzas[i].code + " -> ");
+				System.out.print(pizzas[i].nom + " (");
+				System.out.println(pizzas[i].prix + "€)");
+			}
+		}
+	}
+	
+	/**
+	 * Saisie d'une pizza (pour un ajout ou une modification).
+	 * @param sc Objet Scanner
+	 * @return Objet Pizza
+	 */
+	private static Pizza saisiePizza(Scanner sc) {
+		System.out.println("Veuillez saisir le code");
+		String code = sc.next();
+		System.out.println("Veuillez saisir le nom (sans espace)");
+		String name = sc.next();
+		System.out.println("Veuillez saisir le prix");
+		double price = sc.nextDouble();
+		
+		Pizza pizza = new Pizza();
+		pizza.code = code;
+		pizza.nom = name;
+		pizza.prix = price;
+		
+		return pizza;
+	}
+	
+	/**
+	 * Supprime une pizza et trie le tableau de Pizza.
+	 * @param sc Objet Scanner
+	 * @param pizzas Tableau de Pizza
+	 */
+	private static void supprimerPizza(Scanner sc, Pizza[] pizzas) {
+		int pizzaChoice = sc.nextInt();
+		if (pizzaChoice != 99) {
+			int numPizza = pizzaChoice +1;
+			while (pizzas[numPizza] != null) {
+				pizzas[numPizza -1] = pizzas[numPizza];
+				numPizza++;
+			}
+			pizzas[Pizza.nbPizzas-1] = null;
+		}
+		Pizza.nbPizzas--;
+	}
 }
--- pizzeria-console-objet/src/fr/pizzeria/model/Pizza.java
+++ pizzeria-console-objet/src/fr/pizzeria/model/Pizza.java
@@ -1,5 +1,9 @@
 package fr.pizzeria.model;
 
+/**
+ * Classe Pizza
+ * @author Nicolas
+ */
 public class Pizza {
 	public static int nbPizzas = 0;
 	
@@ -7,4 +11,25 @@ public class Pizza {
 	public String code;
 	public String nom;
 	public double prix;
+	
+	/**
+	 * Constructeur par défaut.
+	 */
+	public Pizza() {
+		
+	}
+	
+	/**
+	 * Constructeur avec paramètres.
+	 * @param id
+	 * @param code
+	 * @param nom
+	 * @param prix
+	 */
+	public Pizza(int id, String code, String nom, double prix) {
+		this.id = id;
+		this.code = code;
+		this.nom = nom;
+		this.prix = prix;
+	}
 }
